281641b41c8fbc3416c43093253800143eb31921
Shveed <vania.shvedkov@yandex.ru>
2019-11-13 18:32:02 +0300

    Почти закончена CategoryActivity ПОИХАЛИ

---
+++ app/src/main/java/com/shveed/cookmegood/CategoryActivity.java
@@ -0,0 +1,15 @@
+package com.shveed.cookmegood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shveed.wallpapperparser.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
