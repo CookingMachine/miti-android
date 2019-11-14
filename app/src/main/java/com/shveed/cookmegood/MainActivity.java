d3538a3231faa17d730d4ee8032d91c913e92b13
Denis Prokopenkov <oprokopenkova8903@list.ru>
2019-11-14 14:20:11 +0300

    Merge branch 'master' of C:\MyProjects\AndroidStudioProjects\CookMeGood1 with conflicts.

--- app/src/main/java/com/shveed/cookmegood/MainActivity.java
+++ app/src/main/java/com/shveed/cookmegood/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private Button goToRecipe;
     private boolean loggedIn = false;
 
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
--- app/src/main/java/com/shveed/cookmegood/RegisterActivity.java
+++ app/src/main/java/com/shveed/cookmegood/RegisterActivity.java
@@ -1,15 +1,33 @@
 package com.shveed.cookmegood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.shveed.wallpapperparser.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+    private boolean loggedIn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.loggedIn = true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void toMainPage(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("loggedIn", loggedIn);
+        startActivity(intent);
+    }
+
+    public void toRegistrationPage(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        intent.putExtra("loggedIn", loggedIn);
+        startActivity(intent);
     }
 }
