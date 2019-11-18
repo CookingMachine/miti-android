82d52aa6202de07c92986b5dcf9728647dea4c18
Иван <vania.shvedkov@yandex.ru>
2019-11-19 02:40:39 +0300

    Фиксы в RecyclerView Нужно добавить поддержку ширины/высоты для элемента

--- app/src/main/java/com/shveed/cookmegood/MainActivity.java
+++ app/src/main/java/com/shveed/cookmegood/MainActivity.java
@@ -7,11 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
