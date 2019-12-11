8eb5fc38d7e0109e237cf62b1a4f804c3a234ff7
Denis Prokopenkov <oprokopenkova8903@list.ru>
2019-12-12 02:33:57 +0300

    Merge branch 'master' of C:\MyProjects\AndroidStudioProjects\Cook with conflicts.

--- app/src/main/java/com/shveed/cookmegood/RecipesGridAdapter.java
+++ app/src/main/java/com/shveed/cookmegood/RecipesGridAdapter.java
@@ -1,6 +1,7 @@
 package com.shveed.cookmegood;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import com.shveed.wallpapperparser.R;
 import java.util.List;
 
 public class RecipesGridAdapter extends RecyclerView.Adapter<RecipesGridAdapter.ViewHolder> {
+
 
     private List<String> mData;
 
@@ -37,6 +39,7 @@ public class RecipesGridAdapter extends RecyclerView.Adapter<RecipesGridAdapter.
         String name = mData.get(position);
         holder.nameView.setText(name);
         holder.amountView.setText(amount);
+
     }
     @Override
     public int getItemCount() {
