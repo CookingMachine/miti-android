91e26e8f1111dd38b6582354aee96cf870170d77
Shveed <vania.shvedkov@yandex.ru>
2020-03-03 15:28:08 +0300

    Начал рефакторинг кода из Java в Kotlin

---
+++ app/src/main/java/com/shveed/cookmegood/adapter/CategoryRecipeAdapter.kt
@@ -0,0 +1,40 @@
+package com.shveed.cookmegood.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shveed.cookmegood.data.dto.Recipe
+import com.shveed.wallpapperparser.R
+import kotlinx.android.synthetic.main.item_category_recipe.view.*
+
+class CategoryRecipeAdapter(private val recipes: List<Recipe>, val context: Context): RecyclerView.Adapter<CategoryRecipeAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+                LayoutInflater.from(context).inflate(
+                        R.layout.item_category_recipe,
+                        parent, false
+                )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return recipes.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val recipe = recipes[position]
+
+        holder.image.setImageResource(recipe.image!!.toInt())
+        holder.name.text = recipe.name
+        holder.kitchen.text = recipe.kitchen
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val image = view.recipeImage!!
+        val name = view.recipeTitle!!
+        val kitchen = view.recipeNationality!!
+    }
+}
---
+++ app/src/main/java/com/shveed/cookmegood/data/dto/Recipe.kt
@@ -0,0 +1,14 @@
+package com.shveed.cookmegood.data.dto
+
+data class Recipe(
+
+        var name: String? = null,
+
+        var description: String? = null,
+
+        var time: String? = null,
+
+        var image: String? = null,
+
+        var kitchen: String? = null
+)
