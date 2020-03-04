c17893bbaea9b9313d982d563388690554bcd240
Anton Bashkin <bashkin415@gmail.com>
2020-03-04 22:54:47 +0300

    Login and Registration activity migrated to Kotlin

---
+++ app/src/main/java/com/shveed/cookmegood/activity/LoginActivity.kt
@@ -0,0 +1,84 @@
+package com.shveed.cookmegood.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.shveed.cookmegood.adapter.LogInDialogAdapter
+import com.shveed.cookmegood.data.NetworkService
+import com.shveed.cookmegood.data.dto.User
+import com.shveed.wallpapperparser.R
+import kotlinx.android.synthetic.main.activity_authorization.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LoginActivity  : AppCompatActivity(), LogInDialogAdapter.LoginDialogListener {
+    var user: User = User()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization)
+        btn_signIn.setOnClickListener { clickSignIn() }
+        btn_signUp.setOnClickListener { clickSignUp() }
+        textAsGuest.setOnClickListener { asGuest() }
+    }
+
+    private fun clickSignIn() {
+        val loginDialog = LogInDialogAdapter()
+        loginDialog.show(supportFragmentManager, "Login dialog")
+    }
+
+    private fun clickSignUp() {
+        val intent = Intent(this, RegActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun asGuest() {
+        val intent = Intent(this, StartActivity::class.java)
+        user = User()
+        startActivity(intent)
+        finish()
+    }
+
+    override fun loginUser(login: String, password: String) {
+        if (login == "" || password == "") {
+            goToast("Пустое поле!")
+        } else {
+            doAuth(login, password)
+        }
+    }
+
+    private fun goToast(output: String) {
+        val errorToast = Toast.makeText(this, output, Toast.LENGTH_SHORT)
+        errorToast.show()
+    }
+
+    private fun doAuth(login: String, pass: String) {
+        NetworkService.getInstance()
+                .userApi
+                .checkUser(login, pass)
+                .enqueue(object : Callback<User?> {
+                    override fun onResponse(
+                            call: Call<User?>,
+                            response: Response<User?>) {
+                        var user = response.body()!!
+                        if (user == null) {
+                            goToast("Неправильный логин или пароль")
+                        } else {
+                            goToast("Добро пожаловать, $login")
+                            val intent = Intent(this@LoginActivity,
+                                    StartActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                    override fun onFailure(
+                            call: Call<User?>,
+                            t: Throwable) {
+                        goToast(t.localizedMessage)
+                    }
+                })
+    }
+}
+
+
+
