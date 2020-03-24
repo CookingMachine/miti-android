c0523e888076c40d1748a67f70db08c159245447
Shveed <vania.shvedkov@yandex.ru>
2020-03-24 21:40:27 +0300

    Переписал IngredientGridAdapter на Kotlin

--- app/src/main/java/com/shveed/cookmegood/data/dto/Ingredient.kt
+++ app/src/main/java/com/shveed/cookmegood/data/dto/Ingredient.kt
@@ -1,6 +1,6 @@
 package com.shveed.cookmegood.data.dto
 
 class Ingredient(s: String, s1: String) {
-    val name: String? = null
-    val amount: String? = null
+    val name: String? = s
+    val amount: String? = s1
 }
