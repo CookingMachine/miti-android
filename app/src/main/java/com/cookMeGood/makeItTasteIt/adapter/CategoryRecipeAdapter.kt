e327a50e481df53ae22ade92396e510c67953b1f
Shveed <vania.shvedkov@yandex.ru>
2020-04-28 16:03:14 +0300

    Поменял слушатель клика по рецепту в категориях

--- app/src/main/java/com/cookMeGood/makeItTasteIt/adapter/CategoryRecipeAdapter.kt
+++ app/src/main/java/com/cookMeGood/makeItTasteIt/adapter/CategoryRecipeAdapter.kt
@@ -7,21 +7,20 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cookMeGood.makeItTasteIt.R
 import com.cookMeGood.makeItTasteIt.data.dto.Recipe
+import com.cookMeGood.makeItTasteIt.listener.OnFragmentChangeListener
+import com.cookMeGood.makeItTasteIt.listener.OnOpenRecipeListener
 import kotlinx.android.synthetic.main.item_category_recipe.view.*
 
-class CategoryRecipeAdapter(private val recipes: List<Recipe>, val context: Context?): RecyclerView.Adapter<CategoryRecipeAdapter.ViewHolder>(){
+class CategoryRecipeAdapter(private val recipes: List<Recipe>, val context: Context?,
+                            private val changeListener: OnOpenRecipeListener):
+        RecyclerView.Adapter<CategoryRecipeAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-                LayoutInflater.from(context).inflate(
-                        R.layout.item_category_recipe,
-                        parent, false
-                )
+        return ViewHolder(LayoutInflater.from(context).inflate(
+                        R.layout.item_category_recipe, parent, false)
         )
     }
 
-    override fun getItemCount(): Int {
-        return recipes.size
-    }
+    override fun getItemCount(): Int = recipes.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
@@ -30,11 +29,18 @@ class CategoryRecipeAdapter(private val recipes: List<Recipe>, val context: Cont
         holder.image.setImageResource(recipe.image!!.toInt())
         holder.name.text = recipe.name
         holder.kitchen.text = recipe.kitchen
+
+        holder.cardLayout.setOnClickListener {
+            val name = recipes[position].name
+            val image = recipes[position].image!!.toInt()
+            changeListener.openRecipe(name!!, image)
+        }
     }
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val image = view.recipeImage!!
         val name = view.recipeTitle!!
         val kitchen = view.recipeNationality!!
+        val cardLayout = view.categoryRecipeLayout!!
     }
 }
---
+++ app/src/main/java/com/cookMeGood/makeItTasteIt/listener/OnOpenRecipeListener.kt
@@ -0,0 +1,7 @@
+package com.cookMeGood.makeItTasteIt.listener
+
+interface OnOpenRecipeListener {
+
+    fun openRecipe(name: String, image: Int)
+
+}
