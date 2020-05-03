2def1841d47ebd7eec983bb62174bb459fa23272
qSonic <bashkin415@gmail.com>
2020-05-03 20:34:21 +0300

    Initial commit
    Signed-off-by: Anton Bashkin <bashkin415@gmail.com>

---
+++ app/src/main/java/com/cookMeGood/makeItTasteIt/activity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.cookMeGood.makeItTasteIt.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cookMeGood.makeItTasteIt.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
