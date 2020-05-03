dcca10c1e5af14eec7b358ce7a9fb2dbda898aec
qSonic <bashkin415@gmail.com>
2020-05-03 22:08:02 +0300

    Добавил сплешскрин
    Signed-off-by: Anton Bashkin <bashkin415@gmail.com>

--- app/src/main/java/com/cookMeGood/makeItTasteIt/activity/SplashActivity.kt
+++ app/src/main/java/com/cookMeGood/makeItTasteIt/activity/SplashActivity.kt
@@ -1,13 +1,30 @@
 package com.cookMeGood.makeItTasteIt.activity
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
 import com.cookMeGood.makeItTasteIt.R
 
-class SplashActivity : AppCompatActivity() {
+private const val SPLASH_TIME_OUT:Long = 3000
+class SplashActivity : SuperActivity() {
+    override fun initInterface() {
+    }
+
+    override fun setAttr() {
+        setLayout(R.layout.activity_splash)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,AuthActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
+
