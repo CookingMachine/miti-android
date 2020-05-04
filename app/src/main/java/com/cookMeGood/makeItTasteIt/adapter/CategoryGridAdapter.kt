d16ee641fba2947e3e13e0904744546f4d8f14d4
Shveed <vania.shvedkov@yandex.ru>
2020-05-04 19:23:29 +0300

    Добавил адаптер парных элеменов CategoryGridAdapter

--- app/src/main/java/com/cookMeGood/makeItTasteIt/adapter/CategoryGridAdapter.kt
+++ app/src/main/java/com/cookMeGood/makeItTasteIt/adapter/CategoryGridAdapter.kt
@@ -1,27 +1,51 @@
 package com.cookMeGood.makeItTasteIt.adapter
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.MarginLayoutParams
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.cookMeGood.makeItTasteIt.R
 import com.cookMeGood.makeItTasteIt.fragment.CategoryFragment
 import com.cookMeGood.makeItTasteIt.listener.OnFragmentChangeListener
 import kotlinx.android.synthetic.main.item_main_recipe.view.*
 
-class CategoryGridAdapter(val context: Context, categoryList: List<String>,
-                          private val changeListener: OnFragmentChangeListener,
-                          private val windowWidth: Int):
+class CategoryGridAdapter(val context: Context):
         RecyclerView.Adapter<CategoryGridAdapter.ViewHolder>() {
 
+    private var items: MutableList<Pair<String, String?>> = mutableListOf()
+    private lateinit var listener : OnFragmentChangeListener
+    private var windowWidth: Int = 0
 
-    private var categoryList: List<String> = categoryList
-        set(value){
-            field = value
-            notifyDataSetChanged()
+    constructor(context: Context, items: List<String>, changeListener: OnFragmentChangeListener,
+                windowWidth: Int) : this(context) {
+        this.listener = changeListener
+        this.windowWidth = windowWidth
+        setItems(items)
+    }
+
+    private fun setItems(items: List<String>) {
+        if(this.items.isNotEmpty()){
+            this.items.clear()
         }
+        for (i in items.indices step 2) {
+            val pair = Pair(
+                    items[i],
+                    if (i + 1 < items.size )
+                        items[i + 1]
+                    else
+                        null
+            )
+            this.items.add(pair)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -31,23 +55,43 @@ class CategoryGridAdapter(val context: Context, categoryList: List<String>,
                 ))
     }
 
-    override fun getItemCount(): Int = categoryList.size
+    override fun getItemCount(): Int = items.size
+
+    private fun fillView(holder: ViewHolder, pair: Pair<String, String?>, first: Boolean) {
+        if(!first && pair.second == null){
+            holder.layoutSecond.visibility = View.GONE
+            return
+        }
+
+        val item = if (first) pair.first else pair.second!!
+
+        if(first){
+            holder.layoutFirst.layoutParams.width = windowWidth / 2 - 45
+            holder.nameFirst.text = item
+            holder.amountFirst.text = "101 позиция"
+            holder.imageFirst.setBackgroundResource(R.drawable.pic2)
+        } else{
+            holder.layoutSecond.layoutParams.width = windowWidth / 2 - 45
+            holder.nameSecond.text = item
+            holder.amountSecond.text = "101 позиция"
+            holder.imageSecond.setBackgroundResource(R.drawable.pic3)
+        }
+
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        holder.layoutFirst.layoutParams.width = windowWidth / 2 - 45
-        holder.layoutSecond.layoutParams.width = windowWidth / 2 - 45
+        val item = items[position]
 
-        holder.nameFirst.text = this.categoryList[position]
-        holder.amountFirst.text = "101 позиция"
-        holder.imageFirst.setBackgroundResource(R.drawable.pic2)
+        fillView(holder, item, true)
+        fillView(holder, item, false)
 
-        holder.nameSecond.text = this.categoryList[position]
-        holder.amountSecond.text = "101 позиция"
-        holder.imageSecond.setBackgroundResource(R.drawable.pic3)
+        holder.layoutFirst.setOnClickListener {
+            listener.replaceFragment(CategoryFragment())
+        }
 
-        holder.layout.setOnClickListener {
-            changeListener.replaceFragment(CategoryFragment())
+        holder.layoutSecond.setOnClickListener {
+            listener.replaceFragment(CategoryFragment())
         }
     }
 
@@ -66,7 +110,7 @@ class CategoryGridAdapter(val context: Context, categoryList: List<String>,
     }
 
     fun onUpdateList(newList: List<String>) {
-        this.categoryList = newList
+        setItems(newList)
         notifyDataSetChanged()
     }
 }
