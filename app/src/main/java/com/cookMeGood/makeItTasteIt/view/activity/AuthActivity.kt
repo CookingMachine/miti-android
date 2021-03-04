d92cbdc6f3e83815abab2f2fe054ccaafa8c7b9c
Shveed <vania.shvedkov@yandex.ru>
2021-03-04 16:49:02 +0300

    Рефакторинг AuthActivity.kt

--- app/src/main/java/com/cookMeGood/makeItTasteIt/view/activity/AuthActivity.kt
+++ app/src/main/java/com/cookMeGood/makeItTasteIt/view/activity/AuthActivity.kt
@@ -1,86 +1,76 @@
 package com.cookMeGood.makeItTasteIt.view.activity
 
-import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.view.View
 import androidx.core.content.ContextCompat
 import com.cookMeGood.makeItTasteIt.R
-import com.cookMeGood.makeItTasteIt.adapter.dialog.LogInDialogAdapter
 import com.cookMeGood.makeItTasteIt.api.ApiService
-import com.cookMeGood.makeItTasteIt.api.model.LoginRequest
-import com.cookMeGood.makeItTasteIt.api.model.LoginResponse
 import com.cookMeGood.makeItTasteIt.api.dto.User
-import com.cookMeGood.makeItTasteIt.utils.ApplicationContext
 import com.cookMeGood.makeItTasteIt.utils.HelpUtils.goToast
+import com.cookMeGood.makeItTasteIt.utils.IntentContainer.INTENT_AUTH
 import kotlinx.android.synthetic.main.activity_auth.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class AuthActivity: SuperActivity(), LogInDialogAdapter.LoginDialogListener {
+class AuthActivity : SuperActivity() {
+
+    override fun initInterface() {
+
+        val isLoggingIn = intent.extras!!.getBoolean(INTENT_AUTH)
+
+        if (isLoggingIn) {
+            authRegisterLayout.visibility = View.GONE
+        }
+        else {
+            authLoginLayout.visibility = View.GONE
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            window.navigationBarColor = ContextCompat.getColor(applicationContext, R.color.colorBlack)
+        }
+
+        authActivityRegisterButton.setOnClickListener {
+            val name = authActivityRegisterName.text.toString()
+            val username = authActivityRegisterLogin.text.toString()
+            val email = authActivityRegisterEmail.text.toString()
+            val password = authActivityRegisterPassword.text.toString()
+
+            if (name.isEmpty()
+                    || username.isEmpty()
+                    || email.isEmpty()
+                    || password.isEmpty()) {
+
+                goToast(applicationContext, "Заполните все поля")
+
+            } else {
+                addUserToServer(User(name, username, password, email, 0))
+            }
+        }
+    }
 
     override fun setAttr() = setLayout(R.layout.activity_auth)
 
-    override fun initInterface() {
-        ApplicationContext.setContext(this)
-
-        window.enterTransition = null
-        window.navigationBarColor = ContextCompat.getColor(applicationContext, R.color.colorBlack)
-        window.statusBarColor = ContextCompat.getColor(applicationContext, R.color.colorBlack)
-
-        btn_signIn.setOnClickListener {
-            val loginDialog = LogInDialogAdapter()
-            loginDialog.show(supportFragmentManager, "Login dialog")
-        }
-
-        btn_signUp.setOnClickListener {
-            intent = Intent(this, RegActivity::class.java)
-            startActivity(intent)
-        }
-
-        textAsGuest.setOnClickListener {
-            intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("user", User(null, "guest", null, null))
-            startActivity(intent)
-            finish()
-        }
+    fun toAuth() {
+        val intent = Intent(this, StartActivity::class.java)
+        startActivity(intent)
     }
 
-    override fun loginUser(login: String, password: String) {
-        if (login == "" || password == "") {
-            goToast(applicationContext, "Пустое поле!")
-        } else {
-            ApiService.getApi()
-                    .authorize(LoginRequest(login, password))
-                    .enqueue(object : Callback<LoginResponse> {
-                        override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                            when (response.code()) {
-                                200 -> {
-                                    getSharedPreferences(ApiService.prefName, Context.MODE_PRIVATE)
-                                            .edit()
-                                            .putString("access_token", response.body()!!.jwtToken)
-                                            .apply()
-
-                                    intent = Intent(applicationContext, SplashActivity::class.java)
-                                    startActivity(intent)
-                                    finish()
-                                }
-                                401 -> {
-                                    goToast(applicationContext, "Неправильный логин или пароль")
-                                }
-                                500 -> {
-                                    goToast(applicationContext, "ОШИБКА СЕРВЕРА")
-                                }
-                            }
+    private fun addUserToServer(user: User){
+        ApiService.getApi()
+                .addUser(user)
+                .enqueue(object : Callback<User> {
+                    override fun onResponse(call: Call<User>, response: Response<User>) {
+                        if (response.isSuccessful) {
+                            val intent = Intent(this@AuthActivity, MainActivity::class.java)
+                            startActivity(intent)
                         }
+                    }
 
-                        override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                            goToast(applicationContext, "Ошибка соединения с сервером")
-                        }
-                    })
-        }
+                    override fun onFailure(call: Call<User>, t: Throwable) {
+                        goToast(applicationContext, "Такой пользователь уже существует")
+                    }
+                })
     }
 }
-
-
-
-
---
+++ app/src/main/java/com/cookMeGood/makeItTasteIt/view/activity/StartActivity.kt
@@ -0,0 +1,48 @@
+package com.cookMeGood.makeItTasteIt.view.activity
+
+import android.content.Intent
+import androidx.core.content.ContextCompat
+import com.cookMeGood.makeItTasteIt.R
+import com.cookMeGood.makeItTasteIt.api.dto.User
+import com.cookMeGood.makeItTasteIt.utils.ApplicationContext
+import com.cookMeGood.makeItTasteIt.utils.IntentContainer.INTENT_AUTH
+import kotlinx.android.synthetic.main.activity_start.*
+
+class StartActivity: SuperActivity() {
+
+    override fun setAttr() = setLayout(R.layout.activity_start)
+
+    override fun initInterface() {
+        ApplicationContext.setContext(this)
+
+        window.enterTransition = null
+        window.navigationBarColor = ContextCompat.getColor(applicationContext, R.color.colorBlack)
+        window.statusBarColor = ContextCompat.getColor(applicationContext, R.color.colorBlack)
+
+        startActivitySignInButton.setOnClickListener {
+            intent = Intent(this, AuthActivity::class.java)
+            intent.putExtra(INTENT_AUTH, true)
+            startActivity(intent)
+            finish()
+        }
+
+        startActivitySignUpButton.setOnClickListener {
+            intent = Intent(this, AuthActivity::class.java)
+            intent.putExtra(INTENT_AUTH, false)
+            startActivity(intent)
+            finish()
+        }
+
+        startActivityAsGuest.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("user", User(null, "guest", null, null))
+            startActivity(intent)
+            finish()
+        }
+    }
+
+}
+
+
+
+
